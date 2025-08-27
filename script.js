85786b7e3e787874d1fc50f20108d3303ea5745e
parbat10 <parbatlimbu139@gmail.com>
2025-08-27 16:22:45 +0545

    just practicing

--- script.js
+++ script.js
@@ -17,4 +17,23 @@
         console.log(message);
        }
        example2();
-       
+  //     console.log(message);
+
+// message is not accessible in out of the function
+
+//3. Block scope
+
+    let condition= true;
+
+    if(condition === true){
+        let age =30;
+        const city = 'btm';
+        var country = 'Nepal'; 
+    }
+    //console.log(age);
+    //console.log(city);
+    console.log(country);
+
+// let and const respect the bloxk scope 
+// var is not block scope - its unction scoped, which can cause bugs in future.
+
